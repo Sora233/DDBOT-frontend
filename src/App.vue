37f357895497a6bece277c444d1dc4ef83a97c44
sora233 <sora@sora233.me>
2021-02-18 16:56:52 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="inner-app">
+    <img alt="Vue logo" src="./assets/logo.png"/>
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite"/>
+    <div class="line"></div>
+    <HelloElePlus msg="Hello Element Plus"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import HelloElePlus from './components/HelloElePlus.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    HelloElePlus
+  }
+})
+</script>
+
+<style scoped>
+.line {
+  height: 1px;
+  background-color: #2f3e4e;
+  margin: 35px -24px;
+}
+
+#inner-app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloElePlus.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <el-button icon="el-icon-success" type="primary" @click="count++">count is: {{ count }}</el-button>
+
+  <p>
+    Edit
+    <code>components/HelloElePlus.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script lang="ts">
+import {ref, defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'HelloElePlus',
+  props: {
+    msg: {
+      type: String,
+      required: true,
+    }
+  },
+  setup: () => {
+    const count = ref(0)
+    return {count}
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
