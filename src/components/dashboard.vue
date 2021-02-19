1708897fd636097a2ee14de41e167f243be3ec2f
sora233 <sora@sora233.me>
2021-02-19 22:48:23 +0800

    update dashboard

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,49 +1,89 @@
 <template>
   <div class="dashboard">
-    <el-row :gutter="20">
-      <el-col :span="6">
+    <el-row :gutter="20" type="flex" justify="space-around">
+      <el-col>
+        <anchor id="group_manage" :level="3">
+          基础信息
+        </anchor>
+      </el-col>
+      <el-col :span="10">
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
               <span style="font-size: xx-large">群数量</span>
             </div>
           </template>
-          <div v-loading="true">
+          <div v-loading="loading">
             {{ groupCount }}
           </div>
         </el-card>
       </el-col>
-      <el-col :span="6">
+      <el-col :span="10">
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
               <span style="font-size: xx-large">好友数量</span>
             </div>
           </template>
-          {{ friendCount }}
+          <div v-loading="loading">
+            {{ friendCount }}
+          </div>
         </el-card>
       </el-col>
     </el-row>
-    <el-row :gutter="20">
-      <el-col :span="6">
+    <el-row :gutter="20" type="flex" justify="space-around">
+      <el-col :span="10">
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
               <span style="font-size: xx-large">触发命令数量</span>
             </div>
           </template>
-          3
+          <div v-loading="loading">
+            0
+          </div>
         </el-card>
       </el-col>
-      <el-col :span="6">
+      <el-col :span="10">
         <el-card class="box-card">
           <template #header>
             <div class="card-header">
               <span style="font-size: xx-large">失败命令数量</span>
             </div>
           </template>
-          4
+          <div v-loading="loading">
+            0
+          </div>
         </el-card>
+      </el-col>
+    </el-row>
+    <el-divider></el-divider>
+    <el-row :gutter="20" type="flex" justify="space-around">
+      <el-col>
+        <anchor id="group_manage" :level="3">
+          群管理
+        </anchor>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :offset="2" :span="20">
+        <el-table stripe>
+          <el-table-column
+              prop="group_code"
+              label="群号码"
+              min-width="120">
+          </el-table-column>
+          <el-table-column
+              prop="group_name"
+              label="群名字"
+              min-width="120">
+          </el-table-column>
+          <el-table-column
+              prop="group_member_count"
+              label="群成员数"
+              min-width="120">
+          </el-table-column>
+        </el-table>
       </el-col>
     </el-row>
   </div>
@@ -61,9 +101,14 @@ export default defineComponent({
   setup() {
     const groupCount = ref(0)
     const friendCount = ref(0)
+    const loading = ref(true)
+    setTimeout(function () {
+      loading.value = false
+    }, 5000)
     return {
       groupCount,
-      friendCount
+      friendCount,
+      loading
     }
   }
 })
